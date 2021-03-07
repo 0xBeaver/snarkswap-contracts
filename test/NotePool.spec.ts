b6777d27025ff3581eaedf3600c58ec42017360b
0xBeaver <0xBeaver@protonmail.com>
2021-03-08 04:33:54 +0900

    testcase notepool

---
+++ test/NotePool.spec.ts
@@ -0,0 +1,107 @@
+import chai, { expect } from "chai";
+import { waffle } from "hardhat";
+import { Contract, Signer, BigNumber, constants } from "ethers";
+import { solidity } from "ethereum-waffle";
+
+import { pairFixture } from "./shared/fixtures";
+import {
+  keccak256,
+  parseEther,
+  randomBytes,
+  defaultAbiCoder,
+} from "ethers/lib/utils";
+import { utils, Note, getNoteHash, eddsa } from "@snarkswap/client";
+import { parse } from "path";
+
+chai.use(solidity);
+
+describe("NotePool", async () => {
+  let wallet: Signer, other: Signer;
+  let walletAddress: string;
+  let notePool: Contract;
+  let token0: Contract;
+  let token1: Contract;
+  let pair: Contract;
+  let privKey: BigNumber;
+  let pubKey: readonly BigNumber[];
+  beforeEach(async () => {
+    [wallet, other] = await waffle.provider.getWallets();
+    walletAddress = await wallet.getAddress();
+    const fixture = await pairFixture(wallet);
+    notePool = fixture.notePool;
+    token0 = fixture.token0;
+    token1 = fixture.token1;
+    pair = fixture.pair;
+    privKey = await utils.genEdDSAPrivKey(pair.address, wallet);
+    pubKey = utils.privToPubKey(privKey);
+    await token0.approve(notePool.address, constants.MaxUint256);
+    await token0.approve(pair.address, constants.MaxUint256);
+  });
+
+  it("deposit", async () => {
+    const amount = parseEther("1");
+    const salt = BigNumber.from(randomBytes(16));
+    const note: Note = {
+      address: token0.address,
+      amount,
+      pubKey,
+      salt,
+    };
+    const noteHash = getNoteHash(note);
+    const pubkeyHash = keccak256(
+      defaultAbiCoder.encode(["uint256", "uint256"], [pubKey[0], pubKey[1]])
+    );
+    await expect(
+      notePool.deposit(token0.address, parseEther("1"), pubKey, salt)
+    )
+      .to.emit(notePool, "Deposit")
+      .withArgs(
+        walletAddress,
+        noteHash,
+        token0.address,
+        amount,
+        pubkeyHash,
+        salt
+      );
+  });
+
+  it("withdraw", async () => {
+    const amount = parseEther("1");
+    const salt = BigNumber.from(randomBytes(16));
+    const note: Note = {
+      address: token0.address,
+      amount,
+      pubKey,
+      salt,
+    };
+    const noteHash = getNoteHash(note);
+    await notePool.deposit(token0.address, parseEther("1"), pubKey, salt);
+    const withdrawSnark = await eddsa.signWithdrawal(
+      noteHash,
+      walletAddress,
+      privKey
+    );
+    const pubkeyHash = keccak256(
+      defaultAbiCoder.encode(["uint256", "uint256"], [pubKey[0], pubKey[1]])
+    );
+    await expect(
+      notePool.withdraw(
+        token0.address,
+        amount,
+        pubKey,
+        salt,
+        walletAddress,
+        withdrawSnark
+      )
+    )
+      .to.emit(notePool, "Withdraw")
+      .withArgs(
+        walletAddress,
+        noteHash,
+        token0.address,
+        amount,
+        pubkeyHash,
+        salt
+      );
+  });
+});
